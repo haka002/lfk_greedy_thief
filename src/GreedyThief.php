6cf71a000bfdf24247ce0641519bc829488ca1b7
Tamas Szabo <szabo.tamas@i-host.hu>
2017-10-19 23:51:05 +0200

    Added the solved kata.

--- src/GreedyThief.php
+++ src/GreedyThief.php
@@ -7,20 +7,56 @@ use InvalidArgumentException;
 class GreedyThief
 {
 	/**
-	 * @param array $items
-	 * @param int   $maximumWeight
+	 * @param array $items    The items what the thief could stealth.
+	 * @param int   $capacity How many kg space is in the bag.
 	 *
 	 * @return array
 	 *
 	 * @throws InvalidArgumentException In case of invalid input.
 	 */
-	public function greedyThief(array $items, $maximumWeight)
+	public function greedyThief(array $items, $capacity)
 	{
-		if (!is_integer($maximumWeight))
+		if (!is_integer($capacity))
 		{
 			throw new InvalidArgumentException('The first argument must be an array and the second one must be an integer, please fix it!');
 		}
 
-		return [];
+		$bag   = [];
+		$items = $this->sortByPrice($items);
+
+		while (
+			$capacity !== 0
+			&& count($items) > 0
+		) {
+			$item = array_pop($items);
+
+			if ($capacity >= $item['weight'])
+			{
+				$bag[]     = $item;
+				$capacity -= $item['weight'];
+			}
+		}
+
+		return $bag;
+	}
+
+	/**
+	 * @param array $items
+	 *
+	 * @return array
+	 */
+	private function sortByPrice(array $items)
+	{
+		usort(
+			$items,
+			function ($item1, $item2)
+			{
+				return $item1["price"] < $item2['price']
+					? -1
+					: 1;
+			}
+		);
+
+		return $items;
 	}
 }
--- test/GreedyThiefTest.php
+++ test/GreedyThiefTest.php
@@ -20,6 +20,8 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 	 * @param array $itemList
 	 * @param int   $maximumWeight
 	 *
+	 * @covers Haka002\GreedyThief\GreedyThief
+	 *
 	 * @dataProvider invalidInputProvider
 	 *
 	 * @expectedException InvalidArgumentException
@@ -29,10 +31,114 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 		$this->greedyThief->greedyThief($itemList, $maximumWeight);
 	}
 
+	/**
+	 * @param array $expected
+	 * @param int   $maximumWeight
+	 * @param array $itemList
+	 *
+	 * @covers Haka002\GreedyThief\GreedyThief
+	 *
+	 * @dataProvider validInputProvider
+	 */
+	public function testValidInput(array $expected, $maximumWeight, array $itemList)
+	{
+		$thiefBag = $this->greedyThief->greedyThief($itemList, $maximumWeight);
+
+		$this->assertEquals(
+			ksort($expected),
+			ksort($thiefBag)
+		);
+	}
+
+	/**
+	 * @return array
+	 */
 	public function invalidInputProvider()
 	{
 		return [
-			[[], 'almafa']
+			[[], 'almafa'],
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function validInputProvider()
+	{
+		return [
+			[
+				[
+					['weight' => 2, 'price' => 6],
+					['weight' => 2, 'price' => 3],
+					['weight' => 4, 'price' => 6],
+				],
+				10,
+				[
+					['weight' => 2, 'price' => 6],
+					['weight' => 4, 'price' => 6],
+
+					['weight' => 2, 'price' => 3],
+					['weight' => 6, 'price' => 5],
+					['weight' => 5, 'price' => 4],
+				]
+			],
+			[
+				[
+					['weight' => 9, 'price' => 5]
+				],
+				10,
+				[
+					['weight' => 9, 'price' => 1],
+					['weight' => 9, 'price' => 2],
+					['weight' => 9, 'price' => 3],
+					['weight' => 9, 'price' => 4],
+					['weight' => 9, 'price' => 5],
+				]
+			],
+			[
+				[
+					['weight' => 2, 'price' => 2],
+					['weight' => 3, 'price' => 3],
+					['weight' => 4, 'price' => 5]
+				],
+				10,
+				[
+					['weight' => 1, 'price' => 1],
+					['weight' => 2, 'price' => 2],
+					['weight' => 3, 'price' => 3],
+					['weight' => 4, 'price' => 4],
+					['weight' => 5, 'price' => 5],
+				]
+			],
+			[
+				[
+					['weight' => 10, 'price' => 10]
+				],
+				10,
+				[
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 10, 'price' => 5],
+					['weight' => 5, 'price' => 5],
+				]
+			],
+			[
+				[
+					[]
+				],
+				8,
+				[
+					['weight' => 9, 'price' => 1],
+					['weight' => 9, 'price' => 2],
+					['weight' => 9, 'price' => 3],
+					['weight' => 9, 'price' => 4],
+					['weight' => 9, 'price' => 5]
+				]
+			],
+
 		];
 	}
 }
