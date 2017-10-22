c66639218e53a2d408c3cf057ac60d4e3718c025
Tamas Szabo <szabo.tamas@i-host.hu>
2017-10-22 12:53:49 +0200

    After te big noob failure, here are the fixes.

--- src/GreedyThief.php
+++ src/GreedyThief.php
@@ -7,22 +7,90 @@ use InvalidArgumentException;
 class GreedyThief
 {
 	/**
-	 * @param array $items    The items what the thief could stealth.
-	 * @param int   $capacity How many kg space is in the bag.
+	 * @param array $items The items what the thief could stealth.
+	 * @param int   $n     How many kg space is in the bag.
 	 *
 	 * @return array
 	 *
 	 * @throws InvalidArgumentException In case of invalid input.
 	 */
-	public function greedyThief(array $items, $capacity)
+	public function greedyThief(array $items, $n)
 	{
-		if (!is_integer($capacity))
+		if (!is_integer($n))
 		{
 			throw new InvalidArgumentException('The first argument must be an array and the second one must be an integer, please fix it!');
 		}
 
+		$capacityDesc       = $n;
+		$collectedBagByDesc = $this->collectItemsByRatio($items, $capacityDesc);
+		$capacityAsc        = $n;
+		$collectedBagByAsc  = $this->collectItemsByRatio($items, $capacityAsc, false);
+
+		return $capacityAsc < $capacityDesc
+			? $collectedBagByAsc
+			: $collectedBagByDesc;
+	}
+
+	/**
+	 * @param array $items
+	 * @param bool  $orderedByDesc
+	 *
+	 * @return array
+	 */
+	private function sortByRatio(array &$items, $orderedByDesc = true)
+	{
+		if ($orderedByDesc)
+		{
+			usort(
+				$items,
+				function ($item1, $item2)
+				{
+					if ($item1["price"] / $item1["weight"] == $item2['price'] / $item2["weight"])
+					{
+						return $item1["price"] < $item2['price']
+							? -1
+							: 1;
+					}
+
+					return $item1["price"] / $item1["weight"] < $item2['price'] / $item2["weight"]
+						? -1
+						: 1;
+				}
+			);
+		}
+		else{
+			usort(
+				$items,
+				function ($item1, $item2)
+				{
+					if ($item1["price"] / $item1["weight"] == $item2['price'] / $item2["weight"])
+					{
+						return $item1["price"] > $item2['price']
+							? -1
+							: 1;
+					}
+
+					return $item1["price"] / $item1["weight"] < $item2['price'] / $item2["weight"]
+						? -1
+						: 1;
+				}
+			);
+		}
+	}
+
+	/**
+	 * Collect the items in the bag ordered by the highest ratio and if them equals order by price.
+	 *
+	 * @param array $items
+	 * @param int   $capacity
+	 * @param bool  $orderByDesc The secondary order direction.
+	 *
+	 * @return array
+	 */
+	private function collectItemsByRatio(array $items, &$capacity, $orderByDesc = true)
+	{
+		$this->sortByRatio($items, $orderByDesc);
 		$bag   = [];
-		$items = $this->sortByPrice($items);
 
 		while (
 			$capacity !== 0
@@ -38,25 +106,5 @@ class GreedyThief
 		}
 
 		return $bag;
-	}
-
-	/**
-	 * @param array $items
-	 *
-	 * @return array
-	 */
-	private function sortByPrice(array $items)
-	{
-		usort(
-			$items,
-			function ($item1, $item2)
-			{
-				return $item1["price"] < $item2['price']
-					? -1
-					: 1;
-			}
-		);
-
-		return $items;
 	}
 }
--- test/GreedyThiefTest.php
+++ test/GreedyThiefTest.php
@@ -44,10 +44,10 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 	{
 		$thiefBag = $this->greedyThief->greedyThief($itemList, $maximumWeight);
 
-		$this->assertEquals(
-			ksort($expected),
-			ksort($thiefBag)
-		);
+		$this->sortByPriceColumn($expected);
+		$this->sortByPriceColumn($thiefBag);
+
+		$this->assertEquals($expected, $thiefBag);
 	}
 
 	/**
@@ -97,9 +97,9 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 			],
 			[
 				[
-					['weight' => 2, 'price' => 2],
-					['weight' => 3, 'price' => 3],
-					['weight' => 4, 'price' => 5]
+					['weight' => 1, 'price' => 1],
+					['weight' => 4, 'price' => 4],
+					['weight' => 5, 'price' => 5],
 				],
 				10,
 				[
@@ -112,7 +112,7 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 			],
 			[
 				[
-					['weight' => 10, 'price' => 10]
+					['weight' => 10, 'price' => 10],
 				],
 				10,
 				[
@@ -121,14 +121,31 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 					['weight' => 2, 'price' => 2],
 					['weight' => 2, 'price' => 2],
 					['weight' => 2, 'price' => 2],
-					['weight' => 10, 'price' => 5],
+					['weight' => 10, 'price' => 10],
 					['weight' => 5, 'price' => 5],
 				]
 			],
 			[
 				[
-					[]
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
 				],
+				10,
+				[
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 2, 'price' => 2],
+					['weight' => 9, 'price' => 9],
+					['weight' => 5, 'price' => 5],
+				]
+			],
+			[
+				[],
 				8,
 				[
 					['weight' => 9, 'price' => 1],
@@ -140,5 +157,26 @@ class GreedyThiefTest extends PHPUnit_Framework_TestCase
 			],
 
 		];
+	}
+
+	/**
+	 * @param array $bag
+	 */
+	private function sortByPriceColumn(array &$bag)
+	{
+		usort($bag, [$this, 'orderByPrice']);
+	}
+
+	/**
+	 * @param array $item1
+	 * @param array $item2
+	 *
+	 * @return int
+	 */
+	private function orderByPrice(array $item1, array $item2)
+	{
+		return $item1["price"] < $item2['price']
+			? -1
+			: 1;
 	}
 }
